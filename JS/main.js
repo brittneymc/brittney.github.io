740973a7f69841aa64ca22dda39133bbff83ab16
Brittney McFarlane <btm44@cornell.ed>
2019-04-03 22:47:18 -0400

    added css and js files

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+	/*--/ Typing Effect /--*/
+	if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+		var typed = new Typed('.text-slider', {
+			strings: typed_strings.split(','),
+			typeSpeed: 80,
+			loop: true,
+			backDelay: 1100,
+			backSpeed: 30
+		});
+	}
+
+})(jQuery);
